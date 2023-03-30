de99bfca99c2d7c236923362c08f0218c19ff386
Pestanas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-30 10:03:02 -0600

    esquematizacion lista de compras con grid

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+
+  return (
+    <div className="container text-center mt-5">
+      <h1>Shopping list</h1>
+      <hr />
+      <div className="row mt-5">
+        <div className="col">C</div>
+        <div className="col-8">Nombre item</div>
+        <div className="col-3">Botones</div>
+      </div>
+
+      <div className="row mt-5">
+        <div className="col">C</div>
+        <div className="col-8">Nombre item</div>
+        <div className="col-3">Botones</div>
+      </div>
+
+      <div className="row mt-5">
+        <div className="col">C</div>
+        <div className="col-8">Nombre item</div>
+        <div className="col-3">Botones</div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
