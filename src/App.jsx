0c63526af0fad29e69444a9dfff4b9ce0bc3d422
Pestanas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-31 10:13:19 -0600

    responsive en pantallas pequeñas y medianas y botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
 function App() {
 
   return (
-    <div className="container text-center mt-5">
-      <h1>Shopping list</h1>
-      <hr />
-      <div className="row mt-5">
-        <div className="col">C</div>
-        <div className="col-8">Nombre item</div>
-        <div className="col-3">Botones</div>
+    <div className="container text-center mt-3">
+      <div className="row">
+        <div className="col text-start">
+          <h1>Shopping list</h1>
+          <hr />
+        </div>
+        <div className="col text-end">
+          <button type="button" class="btn btn-outline-primary bi bi-database-add"></button>
+        </div>
       </div>
 
-      <div className="row mt-5">
-        <div className="col">C</div>
-        <div className="col-8">Nombre item</div>
-        <div className="col-3">Botones</div>
+      <div className="row">
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">1 Kg</div>
+        <div className="col-5 col-md-6 text-start">Tortillas</div>
+        <div className="col-4 col-md-3 btn-group">
+          <button type="button" class="btn btn-outline-warning bi bi-pencil"></button>
+          <button type="button" class="btn btn-outline-secondary bbi bi-files"></button>
+          <button type="button" class="btn btn-outline-danger bi bi-trash-fill"></button>
+        </div>
       </div>
 
-      <div className="row mt-5">
-        <div className="col">C</div>
-        <div className="col-8">Nombre item</div>
-        <div className="col-3">Botones</div>
+      <div className="row">
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">1 Lt</div>
+        <div className="col-5 col-md-6 text-start">Aceite</div>
+        <div className="col-4 col-md-3 btn-group">
+          <button type="button" class="btn btn-outline-warning bi bi-pencil"></button>
+          <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+          <button type="button" class="btn btn-outline-danger bi bi-trash-fill"></button>
+        </div>
       </div>
-      
+
+      <div className="row">
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">3 Lts</div>
+        <div className="col-5 col-md-6 text-start">Refresco</div>
+        <div className="col-4 col-md-3 btn-group">
+          <button type="button" class="btn btn-outline-warning bi bi-pencil"></button>
+          <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+          <button type="button" class="btn btn-outline-danger bi bi-trash-fill"></button>
+        </div>
+      </div>
     </div>
   )
 }
